56606a931b84d66196ba6a6ae32d32f80a97c855
JovanaT99 <66609488+JovanaT99@users.noreply.github.com>
2021-01-12 12:06:04 +0100

    Update Task1Application.java

--- src/main/java/com/example/demo/Task1Application.java
+++ src/main/java/com/example/demo/Task1Application.java
@@ -11,13 +11,10 @@ public class Task1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Task1Application.class, args);
 	}
-
-
-
-	@Bean
-	public RestTemplate getRestTemplate() {
+	
+ @Bean
+ public RestTemplate getRestTemplate() {
+	 
 		return new RestTemplate();
 	}
 }
-
-
